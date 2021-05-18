3a7bdf60c6c8e535880c73541d021026c9171d80
AxelCarlsson <73167102+AxelCarlsson@users.noreply.github.com>
2021-05-18 08:00:56 +0200

    Add files via upload

---
+++ Quiz-master/quiz/src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+public class Main {
+
+    public static void main (String[] args) throws InterruptedException {
+
+        Start start = new Start();
+        start.startApp();
+
+
+    }
+}
